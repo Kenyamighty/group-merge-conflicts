38bf435eba4fb6450a691c6cc73f64f89dfd0a12
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2022-06-12 11:53:16 -0400

    add changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
+function number(...c) {
+    if (!c.every((a) => Numbers.isNumbers(a)))
       throw "All arguments must be numbers.";
     return c.reduce((a, b) => a + b);
   }
   
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+  number(1, 2, 3); //> 6
+  number(10, "B", 20); //> error All arguments must be numbers.
