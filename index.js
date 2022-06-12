d44d9d2bcd62384de8880fb1a0a05355923cc361
Mason Mei <masonmei@pursuit.org>
2022-06-12 10:52:37 -0500

    mason's changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function fn(...c) {
+function addition(...c) {
     if (!c.every((a) => Number.isNumber(a)))
       throw "All arguments must be numbers.";
     return c.reduce((a, b) => a + b);
   }
   
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+  addition(1, 2, 3); //> 6
+  addition(10, "B", 20); //> error All arguments must be numbers.
+  addition("hello", 5, 11); //this should throw error
+  
