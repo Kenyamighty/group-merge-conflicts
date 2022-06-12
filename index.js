c819a133acdb69a1ab929f6510ff0078b12c1db4
KenyaMighty <kenyamighty@pursuit.org>
2022-06-12 11:45:59 -0400

    update function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function fn(...c) {
+function addOrError(...c) {
     if (!c.every((a) => Number.isNumber(a)))
       throw "All arguments must be numbers.";
     return c.reduce((a, b) => a + b);
   }
   
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+  addOrError(1, 2, 3); //> 6
+  addOrError(10, "B", 20); //> error All arguments must be numbers.
